9eafe6887b5ee97c14110fc10c6f5005b5c70b7f
MD Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2023-05-27 00:07:26 +0600

    make recipes data and getting new, popular and bestseller routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const chefs = require("./data/chefs.json");
+const recipes = require("./data/recipes.json");
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -20,6 +22,23 @@ app.get("/api/v1/chef/:name", (req, res) => {
     const name = req.params.name;
     const selectedChef = chefs.find((chef) => chef.name === name);
     res.status(200).send(selectedChef);
+});
+
+app.get("/api/v1/recipes/popular", (req, res) => {
+    const popular = recipes.filter((recipe) => recipe.featured === "Popular");
+    res.status(200).send(popular);
+});
+
+app.get("/api/v1/recipes/bestseller", (req, res) => {
+    const bestseller = recipes.filter(
+        (recipe) => recipe.featured === "BestSeller"
+    );
+    res.status(200).send(bestseller);
+});
+
+app.get("/api/v1/recipes/new", (req, res) => {
+    const newRecipes = recipes.filter((recipe) => recipe.featured === "New");
+    res.status(200).send(newRecipes);
 });
 
 app.listen(port, () => {
